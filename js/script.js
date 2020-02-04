40ed778a8f8ea04289ca260a11591acbd321173c
blakeanneberg <blake.anneberg@gmail.com>
2020-02-03 21:20:42 -0700

    added array data
    and function template

--- js/script.js
+++ js/script.js
@@ -3,13 +3,52 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
+
+var quotes = [
+  {
+      quote: "Generally speaking, the errors in religion are dangerous; those in philosophy only ridiculous.",
+      source: "A Treatise of Human Nature",
+      citation: "David Hume",
+      year: "1739"
+  },
+  {
+    quote: "To write well and to speak well is mere vanity if one does not live well.",
+    source: "Quotation",
+    citation: "Bridget of Sweden",
+    year: "1303-1373"
+  },
+  {
+    quote: "Lagom är bäst.",
+    source: "Quotation",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+    source: "Quotation",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Happiness is not something readymade. It comes from your own actions.",
+    source: "Quotation",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    source: "Sweden",
+    citation: "",
+    year: ""
+  }
+];
 
 
 
@@ -17,6 +56,9 @@ project 1 - A Random Quote Generator
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+
+}
 
 
 /***
