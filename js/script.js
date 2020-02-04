61c5de6c0c1db006b10945a54ef5b6427507d915
blakeanneberg <blake.anneberg@gmail.com>
2020-02-03 21:54:03 -0700

    selects random quote from array

--- js/script.js
+++ js/script.js
@@ -16,56 +16,73 @@ var quotes = [
       quote: "Generally speaking, the errors in religion are dangerous; those in philosophy only ridiculous.",
       source: "A Treatise of Human Nature",
       citation: "David Hume",
-      year: "1739"
+      year: "1739",
+      categorization: "old"
   },
   {
     quote: "To write well and to speak well is mere vanity if one does not live well.",
     source: "Quotation",
     citation: "Bridget of Sweden",
-    year: "1303-1373"
+    year: "1303-1373",
+    categorization: "old"
   },
   {
     quote: "Lagom är bäst.",
     source: "Quotation",
     citation: "",
-    year: ""
+    year: "",
+    categorization: "old"
   },
   {
     quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
     source: "Quotation",
     citation: "",
-    year: ""
+    year: "",
+    categorization: "new"
   },
   {
     quote: "Happiness is not something readymade. It comes from your own actions.",
     source: "Quotation",
     citation: "",
-    year: ""
+    year: "",
+    categorization: "old"
   },
   {
     quote: "It does not matter how slowly you go as long as you do not stop.",
     source: "Sweden",
     citation: "",
-    year: ""
+    year: "",
+    categorization: "old"
   }
 ];
-
+var newArray = [];
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-function getRandomQuote() {
+function getRandomQuote(array) {
+  // Random number generator
+  var quoteIndex = Math.floor(Math.random() * quotes.length);
 
+  for (var i = 0; i < array.length; i++) {
+// array.length rather than the actual quotes variable makes this function a little bit more flexible
+
+    var randomQuote = array[quoteIndex]; 
+    // Random quote variable with the index set to your random number variable
+  }
+  return randomQuote; // Returns random quote variable
 }
-
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() {
+  //call getRandomQuote and print using template in
 
+}
 
 /***
  * click event listener for the print quote button
